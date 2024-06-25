5dd70b9b6204a47b56a819428f65b12c259cd52b
mach90 <132974650+mach90@users.noreply.github.com>
2024-06-25 15:00:34 +0200

    initial commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
+import LogoImg from "../../public/logo.svg";
+
 export default function Logo({ScrollTo}) {
     return (
             <button className="fixed top-0 left-0 m-4 p-2 z-50 bg-customColorCard" onClick={() => ScrollTo('top', 'smooth')}>
-                <img src="../../public/logo.svg" alt="Logo" height="24px" width="24px"/>
+                <img src={LogoImg} alt="Logo" height="24px" width="24px"/>
             </button>
     );
 }

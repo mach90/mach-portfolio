d463ef6778690c698dbb3130854c64e9a77c213d
mach90 <132974650+mach90@users.noreply.github.com>
2024-06-27 10:48:59 +0200

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import PortfolioHeaderImg from "../img/portfolio.svg";
 
 export default function Header() {
     const headerStyle = "flex flex-col justify-start items-center h-screen";
-    const headerH1Style = "font-custom1 text-5xl text-customColorBackground mt-24 mb-4";
-    const headerH2Style = "font-custom1 text-2xl text-customColorBackground";
+    // const headerH1Style = "font-custom1 text-5xl text-customColorBackground mt-24 mb-4";
+    // const headerH2Style = "font-custom1 text-2xl text-customColorBackground";
     const headerBigStyle = "absolute bottom-0 left-0";
 
     return (
         <div className={headerStyle}>
-            <h1 className={headerH1Style}>John Doe</h1>
-            <h2 className={headerH2Style}>Jobtitle namehere</h2>
+            {/* <h1 className={headerH1Style}>Mach90</h1>
+            <h2 className={headerH2Style}>Web things</h2> */}
             <img src={PortfolioHeaderImg} alt="Portfolio" width="100%" className={headerBigStyle}/>
             <SplineBackground />
         </div>
--- src/components/TechLogo.jsx
+++ src/components/TechLogo.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-export default function TechLogo({title, d, link}) {
+export default function TechLogo({title, d, link, icon}) {
     const [isHover, setIsHover] = useState(false);
-    const logoBoxStyle = "flex flex-col justify-center items-center gap-1 cursor-pointer";
+    const logoBoxStyle = "relative flex flex-col justify-center items-center gap-1 cursor-pointer";
     const logoDefaultColor = "#e2e2d1";
     const logoHoverColor = "#f5f5ee";
     const logoSize = "48px";
     const textDefaultStyle = "font-custom2 text-sm text-customColorText";
-    const textHoverStyle = "font-custom2 text-sm text-customColorText";
+    const textHoverStyle = "font-custom2 text-sm text-customColorText ";
 
     function handleHoverEnter(){
         setIsHover(true);
@@ -25,6 +25,7 @@ export default function TechLogo({title, d, link}) {
                     <path d={d} fill={isHover ? logoHoverColor : logoDefaultColor}/>
                 </svg>
                 <p className={isHover ? textHoverStyle : textDefaultStyle}>{title}</p>
+                <div className="text-xs absolute top-[-4px] right-[-4px]">{icon}</div>
             </div>
         </a>
     );
